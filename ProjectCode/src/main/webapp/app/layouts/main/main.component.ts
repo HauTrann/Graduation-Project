9a6027b1e36484e90f71a147c4a7478a76b993f6
Hautv <Hauvantran97@gmail.com>
2020-03-20 01:36:27 +0700

    Update

--- ProjectCode/src/main/webapp/app/layouts/main/main.component.ts
+++ ProjectCode/src/main/webapp/app/layouts/main/main.component.ts
@@ -7,10 +7,12 @@ import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-main',
-  templateUrl: './main.component.html'
+  templateUrl: './main.component.html',
+  styleUrls: ['main.scss']
 })
 export class MainComponent implements OnInit {
   private renderer: Renderer2;
+  isNavbarCollapsed?: boolean;
 
   constructor(
     private accountService: AccountService,
